da76af8a39a19fd9789e0aca4adc85a65bcd63b7
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-11-20 15:15:25 +0100

    Regen android and ios - update build config

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,12 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-  },
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            fallback: "index.html",
+        }),
+    },
 };
 
 export default config;
