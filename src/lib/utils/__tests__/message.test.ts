f0db069a65bba8aa5916cff93bd0f2aaddeaff91
josefinalliende <moalliende@uc.cl>
2025-03-10 12:20:46 -0300

    refactor(messages): add message util to process event into message

--- src/lib/utils/__tests__/message.test.ts
+++ src/lib/utils/__tests__/message.test.ts
@@ -1,5 +1,5 @@
-import { describe, it, expect } from "vitest";
-import { messageHasDeletionTag } from "../message";
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import { messageHasDeletionTag, eventToMessage } from "../message";
 import type { NEvent } from "$lib/types/nostr";
 
 describe("messageHasDeletionTag", () => {
@@ -95,6 +95,139 @@ describe("messageHasDeletionTag", () => {
 
         const messages = [...regularMessages, nonDeletionEvent];
         const result = messageHasDeletionTag(messageId, messages);
-        expect(result).toBe(false);
+        expect(result).toEqual(false);
+    });
+});
+describe("eventToMessage", () => {    
+    const defaultEvent: NEvent = {
+        id: "event123",
+        pubkey: "pubkey456",
+        created_at: 1622548800,
+        kind: 1,
+        tags: [],
+        content: "Hello world",
+        sig: "signature"
+    };
+
+    it('converts event to message', () => {                        
+        const result = eventToMessage(defaultEvent, 'some-other-pubkey');
+        expect(result).toEqual({
+            id: "event123",
+            pubkey: "pubkey456",
+            content: "Hello world",
+            createdAt: 1622548800,
+            replyToId: undefined,
+            reactions: [],
+            lightningInvoice: undefined,
+            lightningPayment: undefined,
+            isSingleEmoji: false,
+            isMine: false,
+            event: defaultEvent
+        });
+    });
+
+    describe('with emojis', () => {
+        it('returns isSingleEmoji true for a single basic emoji', () => {
+             const event = { ...defaultEvent, content :"😊" };
+            const message = eventToMessage(event, "some-pubkey")
+            expect(message.isSingleEmoji).toEqual(true);
+        });
+        
+        it('returns isSingleEmoji true for a compound emoji', () => {
+            const event = { ...defaultEvent, content :"👨‍👩‍👧‍👦" };
+            const message = eventToMessage(event, "some-pubkey")
+            expect(message.isSingleEmoji).toEqual(true);
+        });
+        
+        it('returns isSingleEmoji true for an emoji with skin tone modifier', () => {
+            const event = { ...defaultEvent, content :"👍🏽" };
+            const message = eventToMessage(event, "some-pubkey")
+            expect(message.isSingleEmoji).toEqual(true);
+        });
+        
+        it('returns isSingleEmoji true for emoji with whitespace', () => {
+             const event = { ...defaultEvent, content :" 🎉 "};
+            const message = eventToMessage(event, "some-pubkey")
+            expect(message.isSingleEmoji).toEqual(true);
+        });
+        
+        it('returns isSingleEmoji false for text with emoji', () => {
+            const event = { ...defaultEvent, content :"Hello 👋"};
+            const message = eventToMessage(event, "some-pubkey")
+            expect(message.isSingleEmoji).toEqual(false);
+        });
+        
+        it('returns isSingleEmoji false for multiple emojis', () => {
+             const event = { ...defaultEvent, content :"😊😎"};
+            const message = eventToMessage(event, "some-pubkey")
+            expect(message.isSingleEmoji).toEqual(false);
+        });
+    });
+
+    describe('with same pubkey', () => {
+        it('returns isMine true', () => {
+            const event = { ...defaultEvent, pubkey: "pubkey456" };
+            const message = eventToMessage(event, "pubkey456");
+            expect(message.isMine).toEqual(true);
+        });
+    });
+
+    describe('with reply q tag', () => {
+        it('returns replyToId from q tag', () => {
+            const event = { ...defaultEvent, tags: [["q", "original-event-id"]] };
+            const message = eventToMessage(event, "some-pubkey");
+            expect(message.replyToId).toEqual("original-event-id");
+        });
+    });
+
+    describe('with bolt11 tag', () => {
+        const invoice = "lntbs330n1pnuu4msdqqnp4qg094pqgshvyfsltrck5lkdw5negkn3zwe36ukdf8zhwfc2h5ay6spp5hhsamdzupvqvygycgk5a37fx94m5qctzhz37sf0tensuje9phxgssp50qpmchgkh5z94gffsq3u9sgyr4l778wzj7x4g2wvwtyghdxmt23s9qyysgqcqpcxqyz5vqfuj3u2u2lcs7wdu6k8jh2vur9l3zmffwfup2k8ea7fgeg2puc6xs9cssqcl0xhzngg8z5ye62h3vcgfve56zd9rum2sygndh66qdehgqm4ajkej";
+        const event: NEvent = {
+            id: "event123",
+            pubkey: "pubkey456",
+            created_at: 1622548800,
+            kind: 9734,
+            tags: [["bolt11", invoice, "1000000", "Invoice description"]],
+            content: `Please pay this invoice: ${invoice}`,
+            sig: "signature"
+        };
+        
+    
+        it("sets lightning invoice", () => {
+            const message = eventToMessage(event, "some-pubkey");
+            expect(message.lightningInvoice).toMatchObject({
+                invoice: 'lntbs330n1pnuu4msdqqnp4qg094pqgshvyfsltrck5lkdw5negkn3zwe36ukdf8zhwfc2h5ay6spp5hhsamdzupvqvygycgk5a37fx94m5qctzhz37sf0tensuje9phxgssp50qpmchgkh5z94gffsq3u9sgyr4l778wzj7x4g2wvwtyghdxmt23s9qyysgqcqpcxqyz5vqfuj3u2u2lcs7wdu6k8jh2vur9l3zmffwfup2k8ea7fgeg2puc6xs9cssqcl0xhzngg8z5ye62h3vcgfve56zd9rum2sygndh66qdehgqm4ajkej',
+                amount: 1000,
+                description: "Invoice description",
+                isPaid: false
+            });
+        });
+
+        it('shortens lightning invoice in content', () => {
+            const message = eventToMessage(event, "some-pubkey");
+            expect(message.content).toEqual(
+                'Please pay this invoice: lntbs330n1pnuu4...66qdehgqm4ajkej'
+            );
+        });
+    });
+    
+    describe('with preimage tag', () => {
+        const event: NEvent = {
+            id: "event123",
+            pubkey: "pubkey456",
+            created_at: 1622548800,
+            kind: 9,
+            tags: [["preimage", "preimage123"]],
+            content: "Payment sent",
+            sig: "signature"
+        };
+        it("saves lightning payment", () => {
+            const message = eventToMessage(event, "some-pubkey");
+            
+            expect(message.lightningPayment).toEqual({
+                preimage: 'preimage123',
+                isPaid: false
+            });
+        });
     });
 });
--- src/lib/utils/message.ts
+++ src/lib/utils/message.ts
@@ -1,4 +1,8 @@
-import type { NEvent } from "$lib/types/nostr";
+import type { NEvent } from '$lib/types/nostr';
+import type { Message } from '$lib/types/chat';
+import { findReplyToId } from './tags';
+import { eventToLightningInvoice, eventToLightningPayment } from './lightning';
+
 
 /**
  * Check if a message has been deleted by looking for a kind 5 deletion event
@@ -13,3 +17,42 @@ export function messageHasDeletionTag(messageId: string, messages: NEvent[]): bo
         (m) => m.kind === 5 && m.tags.some((t) => t[0] === "e" && t[1] === messageId)
     );
 }
+
+function isSingleEmoji(str: string) {
+    const trimmed = str.trim();
+    const emojiRegex =
+        /^(?:\p{Emoji_Presentation}|\p{Emoji}\uFE0F)\p{Emoji_Modifier}*(?:\u200D(?:\p{Emoji_Presentation}|\p{Emoji}\uFE0F)\p{Emoji_Modifier}*)*$/u;
+    return emojiRegex.test(trimmed);
+}
+
+function contentToShow(
+    { content, invoice }:
+    { content: string, invoice: string | undefined }
+) {
+    if (!invoice) return content;
+    const firstPart = invoice.substring(0, 15);
+    const lastPart = invoice.substring(invoice.length - 15);
+    return content.replace(invoice, `${firstPart}...${lastPart}`);
+}
+
+export function eventToMessage(event: NEvent, currentPubkey: string | undefined): Message {
+    const replyToId = findReplyToId(event);
+    const isMine = currentPubkey === event.pubkey;
+    const lightningInvoice = eventToLightningInvoice(event);
+    const lightningPayment = eventToLightningPayment(event);
+    const content = contentToShow({ content: event.content, invoice: lightningInvoice?.invoice });
+
+    return {
+        id: event.id,
+        pubkey: event.pubkey,
+        content,
+        createdAt: event.created_at,
+        replyToId,
+        reactions: [],
+        lightningInvoice,
+        isSingleEmoji: isSingleEmoji(content),
+        lightningPayment,
+        isMine,
+        event
+    };
+} 
